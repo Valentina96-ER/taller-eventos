a09f755d4e2511a779cd017e0a508c32e6f4f776
Valentina96-ER <tinaencireg2014@gmail.com>
2024-08-25 15:49:39 -0300

    correccion con StopPropagation

--- eventos.js
+++ eventos.js
@@ -1,5 +1,10 @@
 const botonSaludar = document.getElementById('botonSaludar');
-
+let boton = document.querySelector('button')
 botonSaludar.addEventListener('click', function () {
 alert('Hola! Soy el Div');
+});
+
+boton.addEventListener('click', function(event) {
+    event.stopPropagation();
+    alert('Hola! Soy el botón');
 });
